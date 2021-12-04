1e267c13d225f0ea3bc48158d322526f6485a265
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-12-04 07:01:19 -0300

    explicação sobre o rails no desafio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tarefas
   resources :situacoes
+  resources :tarefas
+  root to: redirect('/swagger.json')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
