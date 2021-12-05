2dac86fdf16e8cd25185825c719014f28baefbcb
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-12-05 06:59:34 -0300

    documentação e white list cross domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  before_action :cross_domain
+
+  def options;end
+
+  private
+    def cross_domain
+      headers['Access-Control-Allow-Origin'] = white_list
+      headers['Vary'] = 'Origin'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, HEAD, DELETE, PUT, PATCH'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['X-Frame-Options'] = 'allow-from *'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, accept, Authorization'
+      headers['Cache-Control'] = "max-age=0, public, no-transform"
+    end
+
+    def white_list
+      lista_liberados = [
+        "http://localhost:3000/",
+        "http://alunos.com.br/",
+        "http://alunos.com.br:3000/",
+        "http://localhost:3001/",
+        "http://localhost:3003/",
+        "http://localhost:3002/"
+      ]
+
+      if lista_liberados.include?(request.referer)
+        return request.referer[0, request.referer.length-1]
+      else
+        request.original_url.remove(request.original_fullpath)
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :situacoes
-  resources :tarefas
   root to: redirect('/swagger.json')
+
+  resources :tarefas
+  match '/tarefas', to: 'application#options', via: [:options]
+  match '/tarefas/:id', to: 'application#options', via: [:options]
+
+  resources :situacoes
+  match '/situacoes', to: 'application#options', via: [:options]
+  match '/situacoes/:id', to: 'application#options', via: [:options]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
